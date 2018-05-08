c439827343bc047316a6e211679a7600748ff41d
Louis Houghton <louis@louishoughton.com>
2018-05-08 12:14:38 +0100

    Fix module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {DataService} from "./data.service";
 import { AppComponent } from './app.component';
-import { BobComponent } from './bob/bob.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BobComponent
   ],
   imports: [
     BrowserModule
